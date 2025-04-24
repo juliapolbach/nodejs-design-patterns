b8f26404d977d132296117794e4700603e94e086
juliapolbach <juliapolbach@gmail.com>
2025-04-24 11:21:52 +0200

    added C3 exercises

---
+++ C5_promises-and-async-await/5.1-dissecting-promise-all/promise-all.mjs
@@ -0,0 +1,4 @@
+// Implement your own version of Promise.all() leveraging promises,
+// async/await, or a combination of the two. The function must be
+// functionally equivalent to its original counterpart.
+
